6530de1827da3b8b63136c761701e2da0ef4cdfc
Reysmejias <reysmejias@gmail.com>
2022-04-06 19:44:24 -0400

    update data.js

--- src/components/Data.js
+++ src/components/Data.js
@@ -2,7 +2,7 @@ export default function data(){
     return{
         banner:{
             id:0,
-            welcome:"Hello Bienvenido a",
+            welcome:"Bienvenido a",
             title:"Web Technology Software",
             content:"Nos enfocamos en desarrollar Aplicaciones o Software a su medida analizando cada requerimiento, estudiando cada punto del proyecto con detenimiento para planificar la mejor ruta de diseño, desarrollo, pruebas y despliegue que se ajuste a sus necesidades.",
         },
